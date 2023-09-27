931f4f646382dcf255ffef9c8da2cea957b8417f
Andy Rea <email@andrewrea.co.uk>
2023-09-27 14:25:03 +0100

    Add unzip functionality
    - Detect if gzipped
    - Unzip
    - Base64 decode

--- main.go
+++ main.go
@@ -2,13 +2,51 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"compress/gzip"
 	"encoding/base64"
 	"fmt"
+	"io"
 	"os"
 )
 
-func decodeBase64(data string) ([]byte, error) {
-	return base64.StdEncoding.DecodeString(data)
+func decodeBase64(data []byte) ([]byte, error) {
+	var output []byte
+	maxDecodedLen := base64.StdEncoding.DecodedLen(len(data))
+	output = make([]byte, maxDecodedLen)
+	n, err := base64.StdEncoding.Decode(output, data)
+	if err != nil {
+		return nil, err
+	}
+
+	return output[:n], nil
+}
+
+func isGzipped(data []byte) bool {
+	if len(data) < 2 {
+		return false
+	}
+	decodedData, err := decodeBase64(data)
+	if err != nil {
+		return false
+	}
+	return len(decodedData) > 1 && decodedData[0] == 0x1F && decodedData[1] == 0x8B
+}
+
+func unzipData(data []byte) ([]byte, error) {
+	decodedData, err := decodeBase64(data)
+	r, err := gzip.NewReader(bytes.NewBuffer(decodedData))
+	if err != nil {
+		return nil, err
+	}
+	return io.ReadAll(r)
+}
+
+func decode(data []byte) ([]byte, error) {
+	if isGzipped(data) {
+		return unzipData(data)
+	}
+	return decodeBase64(data)
 }
 
 func main() {
@@ -16,7 +54,7 @@ func main() {
 
 	if scanner.Scan() {
 		encoded := scanner.Text()
-		decoded, err := decodeBase64(encoded)
+		decoded, err := decode([]byte(encoded))
 		if err != nil {
 			fmt.Println("Failed to decode base64:", err)
 			return
--- main_test.go
+++ main_test.go
@@ -5,8 +5,40 @@ import (
 	"testing"
 )
 
+const (
+	base64EncodedHelloWorld       = "SGVsbG8sIFdvcmxkIQ=="
+	base64EncodedZippedHelloWorld = "H4sIAAAAAAAAA/NIzcnJ11EIzy/KSVEEANDDSuwNAAAA"
+)
+
 func Test_DecodeBase64(t *testing.T) {
-	result, err := decodeBase64("SGVsbG8sIFdvcmxkIQ==")
+	result, err := decodeBase64([]byte(base64EncodedHelloWorld))
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello, World!", string(result))
+}
+
+func Test_IsGzipped(t *testing.T) {
+	encryptedHelloWorld := base64EncodedZippedHelloWorld
+	assert.True(t, isGzipped([]byte(encryptedHelloWorld)))
+}
+
+func Test_Unzip(t *testing.T) {
+	encryptedHelloWorld := base64EncodedZippedHelloWorld
+
+	result, err := unzipData([]byte(encryptedHelloWorld))
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello, World!", string(result))
+}
+
+func Test_Decode_With_Encrypted(t *testing.T) {
+	encryptedHelloWorld := base64EncodedZippedHelloWorld
+	result, err := decode([]byte(encryptedHelloWorld))
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello, World!", string(result))
+}
+
+func Test_Decode(t *testing.T) {
+	encryptedHelloWorld := base64EncodedHelloWorld
+	result, err := decode([]byte(encryptedHelloWorld))
 	assert.Nil(t, err)
 	assert.Equal(t, "Hello, World!", string(result))
 }
