ec796055b0ef05063c9be5e9f7936263299d4f28
Andy Rea <email@andrewrea.co.uk>
2023-10-01 15:12:56 +0100

    Extract the Mime attachments from message
    - Encasulate the main functions into a method to test
    - Extract the boundary
    - Extract the attachments
    - Print out the content type of each attachment

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io"
+	"log"
+	"mime"
+	"mime/multipart"
 	"os"
+	"strings"
 )
 
 func decodeBase64(data []byte) ([]byte, error) {
@@ -49,17 +53,102 @@ func decode(data []byte) ([]byte, error) {
 	return decodeBase64(data)
 }
 
+type MimeAttachment struct {
+	ContentType string
+	Content     []byte
+}
+
+const MediaType = "multipart/mixed; boundary=MIMEBOUNDARY"
+
+func extractBoundary(data []byte) (string, error) {
+	scanner := bufio.NewScanner(bytes.NewReader(data))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "Content-Type:") {
+			_, params, err := mime.ParseMediaType(strings.TrimPrefix(line, "Content-Type: "))
+			if err != nil {
+				return "", err
+			}
+			boundary, found := params["boundary"]
+			if !found {
+				return "", io.EOF
+			}
+			return boundary, nil
+		}
+	}
+	return "", io.EOF
+}
+func decodeMimAttachments(data []byte) (attachments []MimeAttachment, err error) {
+	boundary, err := extractBoundary(data)
+	if err != nil {
+		log.Fatalf("Failed to get boundary: %s", err)
+	}
+
+	reader := multipart.NewReader(bytes.NewReader(data), boundary)
+
+	for {
+		part, err := reader.NextPart()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal("Error reading part: ", err)
+		}
+
+		contentType := part.Header.Get("Content-Type")
+		if contentType == "" {
+			contentType = "application/octet-stream"
+		}
+
+		content, err := io.ReadAll(part)
+		if err != nil {
+			log.Fatal("Error reading content: ", err)
+		}
+
+		encoding := part.Header.Get("Content-Transfer-Encoding")
+		if encoding == "base64" {
+			decoded, err := base64.StdEncoding.DecodeString(string(content))
+			if err != nil {
+				log.Fatal("Error decoding base64: ", err)
+			}
+			content = decoded
+		}
+
+		attachment := MimeAttachment{
+			ContentType: contentType,
+			Content:     content,
+		}
+
+		attachments = append(attachments, attachment)
+	}
+
+	return attachments, err
+}
+
+func extractMimeAttachments(encodedData []byte) (attachments []MimeAttachment, err error) {
+	decoded, err := decode(encodedData)
+	if err != nil {
+
+		return
+	}
+
+	return decodeMimAttachments(decoded)
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	if scanner.Scan() {
 		encoded := scanner.Text()
-		decoded, err := decode([]byte(encoded))
+		attachments, err := extractMimeAttachments([]byte(encoded))
+
 		if err != nil {
-			fmt.Println("Failed to decode base64:", err)
-			return
+			fmt.Println("Failed to extract mime attachments")
 		}
-		fmt.Printf("%s", string(decoded))
+
+		for _, attachment := range attachments {
+			fmt.Printf("Attachment - Media Type: %s \n", attachment.ContentType)
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
--- main_test.go
+++ main_test.go
@@ -42,3 +42,50 @@ func Test_Decode(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "Hello, World!", string(result))
 }
+
+func Test_ExtractMimeAttachments(t *testing.T) {
+	/*  EXAMPLE MESSAGE
+	Content-Type: multipart/mixed; boundary="===============6758256748430075474=="
+	MIME-Version: 1.0
+
+	--===============6758256748430075474==
+	Content-Type: text/cloud-config; charset="utf-8"
+	MIME-Version: 1.0
+	Content-Transfer-Encoding: base64
+	Content-Disposition: attachment; filename="cloud-config.yaml"
+
+	IyBjbG91ZC1jb25maWcueWFtbAp3cml0ZV9maWxlczoKICAjIFdyaXRlIHRoZSBjb25maWd1cmF0
+	aW9uIGZpbGUgdG8gL2V0Yy9zYW1wbGVfYXBwLwogIC0gcGF0aDogIi9ldGMvc2FtcGxlX2FwcC9z
+	b21lLXlhbWwueWFtbCIKICAgIGVuY29kaW5nOiBneitiNjQKICAgIGNvbnRlbnQ6IEg0c0lDQ1ZF
+	RkdVQUEzTnZiV1V0ZVdGdGJDNTVZVzFzQUUyUE1RL0NJQkNGOS82S0M3c0R0R3J0NkdwME1uRTAx
+	M0lxS1VKRGFiWC9Yb3BpR29aM2ZPKzlDOXpjb0h4ZlpRQXJ3SzdURktjYVRUaHhiQjdrM0pTaFVV
+	L1V2MkNEUHFxMDk2aWtxWHVnOFZsdGJSc3pYbmxOdkFMR2QyWEJFaEFCN0IyT0JDZDZ3Y1U2TGY5
+	ZUhyeXpoWVBTR2hDT3RtbVZ1ZGZLaFlTaDNwTzhTdlE0NzI1cDRyTUM5RU1kTDhCRzFBTnh0cUFp
+	VWNHK0hiSHM1TW5ObDUwaTBmRGt4bG9ubFVGUHYwKy9LK0FpaHFjSzhpS3g5U2F4YlpsOUFLdlMy
+	SnhPQVFBQQoKICAjIFdyaXRlIHRoZSB0ZW1wbGF0ZSBmaWxlIHRvIC91c3Ivc2hhcmUvc2FtcGxl
+	X2FwcC8KICAtIHBhdGg6ICIvdXNyL3NoYXJlL3NhbXBsZV9hcHAvc29tZS10ZXh0LnR4dCIKICAg
+	IGVuY29kaW5nOiBneitiNjQKICAgIGNvbnRlbnQ6IEg0c0lDSFZFRkdVQUEzTnZiV1V0ZEdWNGRD
+	NTBlSFFBQXdBQUFBQUFBQUFBQUE9PQoKcnVuY21kOgogIC0gWyIvdXNyL2xvY2FsL2Jpbi9zdGFy
+	dC5zaCJd
+
+	--===============6758256748430075474==
+	Content-Type: text/x-shellscript; charset="utf-8"
+	MIME-Version: 1.0
+	Content-Transfer-Encoding: base64
+	Content-Disposition: attachment; filename="start.sh"
+
+	ZWNobyAiU3RhcnRlZCI=
+
+	--===============6758256748430075474==--
+	*/
+
+	mimeMessage := `
+H4sICAtIFGUAA3VzZXJkYXRhAL2U226jOhSG73mKqPfpGEja0CoX5WBCEkgxZ+6MTYHEHBRIAjz90IOqGe25mK0tbSQLa9nL6/+tz0upqy6turk7NOnTrLywrmjwuftRFn1Kn2dJfakoPg/ru/Xv38PjciUsHx4Xq4UIwONy8bhYr+840zC1uZ+e26Kunmb8PeC4+fxvUjnlNyFd2nc/CKsvdE7q6q3Inmckx+c27dZ3l+5tvvpTqe8jzrhq39LzXKtITYsqe5oluE0fFt871KJt6rboPlJx12GSl1P8efZWsLTCZbq++7X4/YBLdsdxxiAfE13iY4U/JsKyxAG5pAHskpdGJCUDsS9NsZ6Rsd4ZysvRgHTAIWLGBtWxI3/lUJ6UEHA4kC6GHjeJ7mVUX2V7wQfRII1RwN8S3X+LQvm2v9WZoYCM6BBgdZoXEqO6eSUC7Ijes1CAN6JII5cIPNuHLE+C26cixXhXkBm6f4kE6YSDZXUo5CqdPFtH+2vNuiYVYkllPxhaBghgqs3HkEMn6tueNrpVXPi8P9miOtW3quX6sT/C0fa8wdN4tAfW1j5Z+sFZCQ4wRQIQQOIWWCd6MzX+gtyXnjMB6x3e3yEdFoEiRYkoN0iQsCnEr7uRqYewOSYgX8SsQXa4FZ2ReujUHSMfeXiTF/aRnk3QuBj6PrfnfcE8WZq3gb2pngQcdH2w8asDjNtJoUPEJkrK/kBP0ESCPQTaFmINKhYwBhds1ViNxYj3BFePllzs5UMaNnXgyy4SctUVUZf4MR/r8R5D5mLVurlj7lKmAWs0eKKiwfXMJfL4k6vmChqh7FY5IB5sOD+wNjuQF87G5N1geUhU74ZduUR6t5iQuSTQ171NdNsN0n4HYIM38OiMeeOI2dIT4CJiTXvw4J4yc+CcKn+1fSjb9h85AvEHIhBM8w/epvjVUCSeiMaERp6T0vtGhPtkZPV+UGds5Jzq2YOhGFcaWsNetOoo3LLpnyeh3E4A52TzMiVLXezwIA5zsK/Qgn7hxP1bnhwYw3/gpNHA0pHKWa7MHDjZDKlse/CXoUmvk3VSvRfjT4fs8xUEw5dqob9GAmz3wrZJCmmkOhw4qixHrGzpf2k4/bzNU8Zaci6a7n/vOG03Nd77Np8aTRxYdTK8FJ6IcjJdaawY6781Np9z3E+mf/Qm1gUAAA==
+`
+	attachments, err := extractMimeAttachments([]byte(mimeMessage))
+
+	assert.Nil(t, err)
+	assert.Len(t, attachments, 2)
+	assert.Equal(t, attachments[0].ContentType, "text/cloud-config; charset=\"utf-8\"")
+	assert.Equal(t, attachments[1].ContentType, "text/x-shellscript; charset=\"utf-8\"")
+}
