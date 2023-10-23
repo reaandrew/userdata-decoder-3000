31d3705dff6ff31081c44fdd482c686bf2867562
Andy Rea <email@andrewrea.co.uk>
2023-10-23 17:07:50 +0100

    Test returns different providers

--- Config_test.go
+++ Config_test.go
@@ -9,3 +9,26 @@ func TestHandlesUnknownProvider(t *testing.T) {
 	_, err := Config{providerKey: "talula"}.getProvider()
 	assert.ErrorContains(t, err, "unknown provider")
 }
+
+func TestReturnsCommandLineProvider(t *testing.T) {
+	provider, err := Config{
+		outputDir:   "",
+		providerKey: "",
+		args: []string{
+			"something",
+		},
+	}.getProvider()
+
+	assert.Nil(t, err)
+	assert.IsType(t, CommandLineProvider{}, provider)
+}
+
+func TestReturnsAwsProvider(t *testing.T) {
+	provider, err := Config{
+		outputDir:   "",
+		providerKey: "aws",
+	}.getProvider()
+
+	assert.Nil(t, err)
+	assert.IsType(t, AWSProvider{}, provider)
+}
