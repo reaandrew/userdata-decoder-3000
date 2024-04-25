1973bf190f0e25e7057c4bc8b49e3188fead0dc4
Andy Rea <email@andrewrea.co.uk>
2024-04-25 13:43:35 +0100

    Changing to a loop on windows

--- scripts/ci/test_windows.ps1
+++ scripts/ci/test_windows.ps1
@@ -5,9 +5,8 @@ param (
 
 # Execute the command with parameters
 & $Executable --provider aws -v --output-dir ./output/$Os/
-$files = Get-ChildItem -Path .\output\$Os -Recurse -File | Select-Object -ExpandProperty FullName
-$fileCount = $files.Count
-Write-Output "Number of files: $fileCount"
+$files = Get-ChildItem -Path .\output\$Os -Recurse -File | Select-Object -ExpandProperty Name
+Write-Output "Files found: $($files -join ', ')"
 
 # Read required files from a list file
 $requiredFiles = Get-Content -Path .\scripts\ci\expected_files.txt
@@ -17,14 +16,9 @@ Write-Output "Required files are:"
 $requiredFiles | ForEach-Object { Write-Output $_ }
 
 # Check for each required file and exit with an error if any are missing
-$foundFiles = $files | Where-Object { $requiredFiles -contains $_.Name }
-
-if ($foundFiles.Count -ne $requiredFiles.Count) {
-    Write-Error "Error: Required files are missing!"
-    exit 1
-}
-
-if ($fileCount -ne 6) {
-    Write-Error "Error: Incorrect number of files found! Expected 6, found $fileCount"
-    exit 1
+foreach ($file in $requiredFiles) {
+    if (-not ($files -contains $file)) {
+        Write-Error "Error: Can't find $file"
+        exit 1
+    }
 }
